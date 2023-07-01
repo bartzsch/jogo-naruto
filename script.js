f0b906dd91a421aea1b363f029138f4771499f31
bartzsch <123250281+bartzsch@users.noreply.github.com>
2023-07-01 09:36:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const personagem = document.querySelector(".mario")
 const obstaculo = document.querySelector(".pipe")
+const pontuacao = document.querySelector(".pontuacao")
 //console.log(obstaculo)   
 const pulo = () => {
     personagem.classList.add('jump')
@@ -7,26 +8,28 @@ const pulo = () => {
         personagem.classList.remove('jump')
     }, 700)
 }
-const looping = setInterval(()=>{
-const obstaculoPosition=obstaculo.offsetLeft
-const personagemPosition=+window.getComputedStyle(personagem).bottom.replace('px','')
-if(obstaculoPosition<=90 && obstaculoPosition>0 && personagemPosition<=77){
-obstaculo.style.animation='none'
-obstaculo.style.left=`${obstaculoPosition}px`
-personagem.style.animation='none'
-personagem.style.bottom=`${personagemPosition}px`
-personagem.src='game_over.gif'
-personagem.style.width='90%'
-clearInterval(looping)
-}
-console.log(personagemPosition)
-},10)
+const looping = setInterval(() => {
+    const obstaculoPosition = obstaculo.offsetLeft
+    const personagemPosition = +window.getComputedStyle(personagem).bottom.replace('px', '')
+    if (obstaculoPosition <= 90 && obstaculoPosition > 0 && personagemPosition <= 77) {
+        obstaculo.style.animation = 'none'
+        obstaculo.style.left = `${obstaculoPosition}px`
+        personagem.style.animation = 'none'
+        personagem.style.bottom = `${personagemPosition}px`
+        personagem.src = 'game_over.gif'
+        personagem.style.width = '90%'
+        clearInterval(looping)
+    } else{
+        pontuacao.innerHTML++;
+    }
+    console.log(personagemPosition)
+}, 10)
 
 
 
 
 
-document.addEventListener('keydown',pulo)
+document.addEventListener('keydown', pulo)
 
 
 
